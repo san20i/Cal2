0e96d9b16bfceacd98a971c45effb777b548d5b2
sani14470 <sani14470@gmail.com>
2019-09-26 19:31:24 +0300

    Привет World

---
+++ cal2/src/cal2.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : cal2.c
+ Author      : Alexandr
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main() {
+  setvbuf (stdout, NULL, _IONBF, 0);
+  setvbuf (stdin, NULL, _IONBF, 0);
+   float x,y;
+   char sign='+';
+   while (sign != '0') {
+      printf("Знак: ");
+      scanf("%c%*c", &sign);
+      if (sign == '0') { break; }
+      if (sign == '+' || sign == '-' || sign == '*' || sign == '/') {
+         printf("x=");
+         scanf("%f%*c", &x);
+         printf("y=");
+         scanf("%f%*c", &y);
+         switch (sign) {
+            case '+':
+               printf("%.2f\n", x+y);
+               break;
+            case '-':
+               printf("%.2f\n", x-y);
+               break;
+            case '*':
+               printf("%.2f\n", x*y);
+               break;
+            case '/':
+               if (y != 0) printf("%.2f\n", x/y);
+               else printf("Деление на ноль!\n");
+         }
+      }
+      else printf("Неверно указан знак операции\n");
+   }
+}
